ac2bc713c5607a810a062bf7812d975b1d62580e
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2019-01-21 01:07:13 +0900

    impl giving command line arguments to main/1

--- vm/src/cmd.rs
+++ vm/src/cmd.rs
@@ -3,24 +3,35 @@ use clap::App;
 use config::{set_global, Config};
 use interp::Interp;
 use interp_init;
+use list::List;
+use std::process;
 use std::sync::Arc;
 use value::Value;
 
 pub struct Command {
     pub interp: Arc<Interp>,
+    pub args: Vec<String>,
 }
 
 impl Command {
     pub fn new() -> Command {
         Command {
             interp: Arc::new(interp_init::init()),
+            args: Vec::new(),
         }
     }
 
     pub fn run(&mut self) {
         let yaml = load_yaml!("cli.yml");
-        let matches = App::from_yaml(yaml).get_matches();
-        let input = matches.value_of("INPUT").unwrap();
+        let mut app = App::from_yaml(yaml);
+        let matches = app.clone().get_matches();
+        let mut input = match matches.values_of("INPUT") {
+            Some(input) => input,
+            None => {
+                app.print_long_help().unwrap();
+                process::exit(1)
+            }
+        };
 
         let config = Config {
             debug_mode: matches.is_present("debug"),
@@ -28,7 +39,12 @@ impl Command {
         };
         set_global(config);
 
-        match bytecode::from_file(input) {
+        let file = input.next().unwrap();
+        let args: Vec<Value> = input
+            .map(|arg| Value::String(Arc::new(arg.to_string())))
+            .collect();
+
+        match bytecode::from_file(file) {
             Err(msg) => panic!("Error: invalid bytecode format: {}", msg),
             Ok(bc) => {
                 debug!("execute module initialization function");
@@ -52,7 +68,16 @@ impl Command {
                 {
                     match m.desc.fields.get("main") {
                         Some(Value::CompiledCode(code)) => {
-                            match Interp::eval(&interp, &proc, None, code.clone(), Vec::new()) {
+                            let arity = (*code).arity;
+                            let res = if arity == 0 {
+                                Interp::eval(&interp, &proc, None, code.clone(), Vec::new())
+                            } else if arity == 1 {
+                                let cmd_args = List::value_from_list(&proc.heap, args);
+                                Interp::eval(&interp, &proc, None, code.clone(), vec![cmd_args])
+                            } else {
+                                panic!("main/0 or main/1 not found")
+                            };
+                            match res {
                                 Ok(value) => debug!("# main => {:?}", value),
                                 Err(e) => println!("# error: {:?}", e),
                             }
