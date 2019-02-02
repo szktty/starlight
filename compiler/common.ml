a70ff0935f2eb4fc0e3b08070262f5d4c8264b29
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2019-02-03 00:37:24 +0900

    add sexp utilities

--- compiler/common.ml
+++ compiler/common.ml
@@ -13,3 +13,15 @@ end
 let extract = Seplist.values
 let printf = Printf.printf
 let sprintf = Printf.sprintf
+
+let satom name = Sexp.Atom name
+let slist list = Sexp.List list
+let spair name value = Sexp.List [satom name; value]
+let sconcat list =
+  List.fold_left list
+    ~init:[]
+    ~f:(fun accu e -> match e with
+        | Some e -> e :: accu
+        | None -> accu)
+  |> List.rev
+  |> slist
